d2079639292186d9bd4071c2b3a4c93a0d2d9f89
Bo Wang <Bo.Wang@Home>
2009-02-05 01:04:32 +0800

    在创建 purchase_order 的 store_items 的时候，记录 purchase_order，否则会缺 purchase_order_id。

--- app/models/purchase_order.rb
+++ app/models/purchase_order.rb
@@ -4,6 +4,7 @@ class PurchaseOrder < ActiveRecord::Base
 
   def new_store_item_attributes=(store_item_attributes)
     store_item_attributes.each do |attributes|
+      attributes[:purchase_order_id] = self
       store_items.build(attributes)
     end
   end
--- app/models/store_item.rb
+++ app/models/store_item.rb
@@ -2,6 +2,8 @@ class StoreItem < ActiveRecord::Base
   belongs_to :purchase_order
   belongs_to :vendor_product
 
-  validates_presence_of :purchase_order_id
-  validates_presence_of :vendor_product_id
+  validates_presence_of :purchase_order_id,
+                        :message => "Missing purchase_order_id"
+  validates_presence_of :vendor_product_id,
+                        :message => "Missing vendor_product_id"
 end
