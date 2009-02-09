e76359424b43d2bdf6a231b09cdbaccce2055e10
Bo Wang <Bo.Wang@Office>
2009-02-09 14:23:18 +0800

    增加 Vendor 的 validation。

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -1,4 +1,7 @@
 class Vendor < ActiveRecord::Base
+  validates_presence_of :full_name, :currency_id, :active
+  validates_uniqueness_of :full_name, :abbr_name
+
   belongs_to :currency
   has_many :vendor_products
 end
