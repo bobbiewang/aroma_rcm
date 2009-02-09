1dde44ff305dd16fe580bbf4d0c5883b0925b27c
Bo Wang <Bo.Wang@Office>
2009-02-09 14:21:54 +0800

    增加 Currency 的 validation 和 unit test。

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -1,3 +1,4 @@
 class Currency < ActiveRecord::Base
   validates_presence_of :full_name, :iso_code, :symbol
+  validates_uniqueness_of :full_name, :iso_code
 end
--- test/unit/currency_test.rb
+++ test/unit/currency_test.rb
@@ -1,12 +1,36 @@
 require 'test_helper'
 
 class CurrencyTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
   def test_invalid_with_empty_attributes
     currency = Currency.new
     assert !currency.valid?, "Empty Currency should be invalid."
     assert currency.errors.invalid?(:full_name), "Currency.full_name shouldn't be empty."
     assert currency.errors.invalid?(:iso_code), "Currency.iso_code shouldn't be empty."
     assert currency.errors.invalid?(:symbol), "Currency.symbol shouldn't be empty."
+  end
+
+  def test_unique_full_name
+    currency = Currency.new(:full_name => currencies(:cny).full_name,
+                            :iso_code  => "test",
+                            :symbol    => "test")
+    assert !currency.valid?
+    assert_equal ActiveRecord::Errors.default_error_messages[:taken],
+                 currency.errors.on(:full_name)
+  end
+
+  def test_unique_iso_code
+    currency = Currency.new(:full_name => "test",
+                            :iso_code  => currencies(:cny).iso_code,
+                            :symbol    => "test")
+    assert !currency.valid?
+    assert_equal ActiveRecord::Errors.default_error_messages[:taken],
+                 currency.errors.on(:iso_code)
+  end
+
+  def test_multiple_symbol
+    currency = Currency.new(:full_name => "test",
+                            :iso_code  => "test",
+                            :symbol    => currencies(:cny).symbol)
+    assert currency.valid?
   end
 end
