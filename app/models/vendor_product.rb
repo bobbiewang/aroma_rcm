cdf664be48a49938892bec9e0867aff5ebf4f86d
Bo Wang <Bo.Wang@Office>
2009-02-09 16:08:11 +0800

    增加 VendorProduct 的 validation 和 unit test。

--- app/models/vendor_product.rb
+++ app/models/vendor_product.rb
@@ -1,3 +1,7 @@
 class VendorProduct < ActiveRecord::Base
+  validates_presence_of :title, :vendor_id, :price, :active
+  validates_uniqueness_of :title
+  validates_numericality_of :vendor_id, :capacity, :price
+
   belongs_to :vendor
 end
--- test/unit/vendor_product_test.rb
+++ test/unit/vendor_product_test.rb
@@ -1,8 +1,50 @@
+# -*- coding: utf-8 -*-
 require 'test_helper'
 
 class VendorProductTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  def test_capacity_should_be_number
+    ok = %w{ 1 1.0 99 99.9 }
+    bad = %w{ 1。0 abc }
+
+    ok.each do |cap|
+      vp = VendorProduct.new(:title => "test",
+                             :vendor_id => 1,
+                             :capacity => cap,
+                             :price => 1.1,
+                             :active => true)
+      assert vp.valid?, vp.errors.full_messages
+    end
+
+    bad.each do |cap|
+      vp = VendorProduct.new(:title => "test",
+                             :vendor_id => 1,
+                             :capacity => cap,
+                             :price => 1.1,
+                             :active => true)
+      assert !vp.valid?, "capacity is #{cap}, which should be invalid."
+    end
+  end
+
+  def test_price_should_be_number
+    ok = %w{ 1 1.0 99 99.9 }
+    bad = %w{ 1。0 abc }
+
+    ok.each do |price|
+      vp = VendorProduct.new(:title => "test",
+                             :vendor_id => 1,
+                             :capacity => 1.1,
+                             :price => price,
+                             :active => true)
+      assert vp.valid?, vp.errors.full_messages
+    end
+
+    bad.each do |price|
+      vp = VendorProduct.new(:title => "test",
+                             :vendor_id => 1,
+                             :capacity => 1.1,
+                             :price => price,
+                             :active => true)
+      assert !vp.valid?, "price is #{price}, which should be invalid."
+    end
   end
 end
