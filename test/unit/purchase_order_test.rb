b464f26feb70c30e23d278a12378f2693d9aa482
Bo Wang <Bo.Wang@Office>
2009-02-10 15:23:24 +0800

    设计 PurchaseOrder 中计算 PurchaseOrderItem 的 cost 的测试数据和代码。

--- test/unit/purchase_order_test.rb
+++ test/unit/purchase_order_test.rb
@@ -1,8 +1,22 @@
-require 'test_helper'
+require File.dirname(__FILE__) + '/../test_helper'
 
 class PurchaseOrderTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+
+  def test_calculate_cost_of_items
+    po = purchase_orders(:from_ppa)
+    items = po.purchase_order_items
+
+    assert_equal 2,    items.size
+    assert_equal 5.0,  items[0].unit_price
+    assert_equal 2,    items[0].quantity
+    assert_equal 10.0, items[1].unit_price
+    assert_equal 1,    items[1].quantity
+
+    po.postage = 20
+    po.total_cost = 200
+    po.save
+
+    assert_equal 50.0,  items[0].unit_cost
+    assert_equal 100.0, items[1].unit_cost
   end
 end
--- test/unit/vendor_test.rb
+++ test/unit/vendor_test.rb
@@ -2,11 +2,7 @@
 require 'test_helper'
 
 class VendorTest < ActiveSupport::TestCase
-  def test_valide_vendors
-    vendor = Vendor.create(:full_name   => "Penny Price",
-                           :abbr_name   => "PPA",
-                           :currency_id => 2,
-                           :active      => false)
-    assert_valid(vendor)
+  def test_true
+    assert true
   end
 end
