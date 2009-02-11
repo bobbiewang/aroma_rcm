574c21afae200b923d82515e7c32bfafbbaa53ef
Bo Wang <Bo.Wang@Office>
2009-02-11 11:07:12 +0800

    更新 PurchaseOrderItem 时，同时更新其 SaleOrderItem 的 unit_cost。

--- app/models/purchase_order_item.rb
+++ app/models/purchase_order_item.rb
@@ -6,6 +6,9 @@ class PurchaseOrderItem < ActiveRecord::Base
 
   belongs_to :purchase_order
   belongs_to :vendor_product
+  has_many :sale_order_items
+
+  after_update :update_sale_order_item_costs
 
   def total_price
     # 如果 unit_price 是 nil，返回 nil；否则正常计算
@@ -15,5 +18,15 @@ class PurchaseOrderItem < ActiveRecord::Base
 
   def avail_quantity
     quantity
+  end
+
+  protected
+  def update_sale_order_item_costs
+    return unless unit_cost
+
+    sale_order_items.each do |item|
+      item.unit_cost = self.unit_cost
+      item.save
+    end
   end
 end
--- test/unit/purchase_order_item_test.rb
+++ test/unit/purchase_order_item_test.rb
@@ -1,4 +1,5 @@
-require 'test_helper'
+# -*- coding: utf-8 -*-
+require File.dirname(__FILE__) + '/../test_helper'
 
 class PurchaseOrderItemTest < ActiveSupport::TestCase
   def test_valid_purchase_order_items
@@ -7,5 +8,30 @@ class PurchaseOrderItemTest < ActiveSupport::TestCase
                                                 :unit_price => 1.1,
                                                 :quantity => 1)
     assert_valid(purchase_order_item)
+  end
+
+  def test_should_update_sale_order_items
+    poi = purchase_order_items(:purchase_4_ppa_oil)
+    sois = poi.sale_order_items
+
+    poi.unit_cost = 10.0
+    poi.save
+    assert_equal poi.unit_cost, sois[0].unit_cost
+
+    poi.unit_cost = 20.0
+    poi.save
+    assert_equal poi.unit_cost, sois[0].unit_cost
+  end
+
+  def test_should_not_update_sale_order_items
+    # 如果 PurchaseOrderItem.unit_cost 为空，不更新其 SaleOrderItem.uni_cost
+    poi = purchase_order_items(:purchase_1_cream)
+    sois = poi.sale_order_items
+
+    sois[0].unit_cost = 199.9
+    sois[0].save
+    poi.unit_cost = nil
+    poi.save
+    assert_equal 199.9, sois[0].unit_cost
   end
 end
