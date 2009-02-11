a38bd6cbba89ba50d399409be9447e75e6f9df82
Bo Wang <Bo.Wang@Home>
2009-02-11 22:54:49 +0800

    使用基于 Active Record 的 session 机制。

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
 
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
-  protect_from_forgery # :secret => '3b19ee5191055215d777a93fdab303b6'
+  protect_from_forgery :secret => '3b19ee5191055215d777a93fdab303b6'
 
   # See ActionController::Base for details
   # Uncomment this to filter the contents of submitted sensitive data parameters
