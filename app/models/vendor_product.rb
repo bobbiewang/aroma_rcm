0b7e91e78581b8bb9d1c672522c3c2bef2d45236
Bo Wang <Bo.Wang@Home>
2009-02-15 01:00:57 +0800

    不同 vendor 可以有同名产品。

--- app/models/vendor_product.rb
+++ app/models/vendor_product.rb
@@ -1,9 +1,12 @@
 class VendorProduct < ActiveRecord::Base
   validates_presence_of :title, :vendor_id, :active
-  validates_uniqueness_of :title
+  validates_uniqueness_of :title, :case_sensitive => false, :scope => "vendor_id"
   validates_numericality_of :vendor_id
   validates_numericality_of :capacity, :allow_nil => true
   validates_numericality_of :price, :allow_nil => true
+
+  # TODO 创建 title 失败 3ml Chamomile Roman，重定向后找不到 vendors
+  # TODO 在 conroller 中测试 title 在 group 中的 uniqueness
 
   belongs_to :vendor
 end
--- test/unit/vendor_product_test.rb
+++ test/unit/vendor_product_test.rb
@@ -2,6 +2,23 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class VendorProductTest < ActiveSupport::TestCase
+  def test_duplicate_title
+    # 同一个 vendor 不能有同名的产品
+    vp = VendorProduct.new(:title => vendor_products(:oil).title,
+                           :vendor_id => vendor_products(:oil).id,
+                           :capacity => 1,
+                           :price => 9.9,
+                           :active => true)
+    assert !vp.valid?, "Duplicate title."
+    assert vp.errors.invalid?(:title)
+    assert_equal ActiveRecord::Errors.default_error_messages[:taken],
+                 vp.errors.on(:title)
+
+    # 不同 vendor 可以有同名的产品
+    vp.vendor_id = vendor_products(:oil).id + 1
+    assert vp.valid?, vp.errors.full_messages
+  end
+
   def test_capacity_should_be_number_or_nil
     nums = %w{ 1 1.0 99 99.9 }
     nans = %w{ 1。0 abc }
