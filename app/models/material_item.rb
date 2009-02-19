223cfb0596dfe75daedde4e0de6a0d76e13ce578
Bo Wang <Bo.Wang@Office>
2009-02-19 11:02:46 +0800

    在 /materials/ 页面显示原料的数量和价格。

--- app/models/material_item.rb
+++ app/models/material_item.rb
@@ -6,4 +6,12 @@ class MaterialItem < ActiveRecord::Base
 
   belongs_to :purchase_order
   belongs_to :vendor_product
+
+  def unit_cost
+    item_cost / vendor_product.material_amount
+  end
+
+  def total_material_amount
+    quantity * vendor_product.material_amount
+  end
 end
