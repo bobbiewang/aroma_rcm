54fb88c916fbf7ff2858b63a1d6b4d34e27f367f
Bo Wang <Bo.Wang@Office>
2009-02-25 15:03:20 +0800

    增加 MaterialItem 的测试。

--- test/unit/material_item_test.rb
+++ test/unit/material_item_test.rb
@@ -1,8 +1,14 @@
-require 'test_helper'
+require File.dirname(__FILE__) + '/../test_helper'
 
 class MaterialItemsTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  def test_cost
+    purchase_orders(:purchase_from_ppa).save
+
+    mi = material_items(:purchase_2_ppa_oil)
+    assert_equal 2,    mi.quantity
+    assert_equal 30,   mi.item_cost
+    assert_equal 60,   mi.total_cost
+    assert_equal 400,  mi.total_material_amount
+    assert_equal 0.15, mi.unit_cost
   end
 end
