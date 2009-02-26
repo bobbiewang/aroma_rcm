a07e4c28888d864cd5f29cfaded3ca93c5b243c2
Bo Wang <Bo.Wang@Office>
2009-02-26 11:12:35 +0800

    计算 SaleOrderItem 的 unit cost 的时候考虑 PurchaseOrderItem 的成本未定的情况（向前兼容）。

--- app/models/sale_order_item.rb
+++ app/models/sale_order_item.rb
@@ -1,4 +1,4 @@
-# -*- coding: undecided -*-
+# -*- coding: utf-8 -*-
 class SaleOrderItem < ActiveRecord::Base
   belongs_to :sale_order
   belongs_to :purchase_order_item
@@ -14,7 +14,7 @@ class SaleOrderItem < ActiveRecord::Base
   end
 
   def unit_cost
-    # 1.x 版本中，把自制产品也作为 vendor product，所以进货的成本可能为 0
+    # 1.x 版本中，把自制产品也作为 vendor product，所以进货的成本可能为空（未定）
     purchase_order_item.unit_cost || 0.0
   end
 
