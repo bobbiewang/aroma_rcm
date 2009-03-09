d9a030c6d9942fd88b504b6495df2ae716a04953
Bo Wang <Bo.Wang@Office>
2009-03-09 10:28:22 +0800

    实现 Customer 按照名字排序功能。

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,11 @@
 class Customer < ActiveRecord::Base
+  include Comparable
+
   has_many :sale_orders, :order => 'saled_at'
+
+  def <=>(other)
+    utf8_2_gbk(name) <=> utf8_2_gbk(other.name)
+  end
 
   def total_expense
     sale_orders.inject(0) { |sum, i| sum += i.total_price }
